4e49213c769844060e8ac55e1c55760acf507f09
Alexandra Dinca <alexandradi2003@gmail.com>
2023-12-06 19:53:21 +0200

    Testing 2

--- src/parallel.c
+++ src/parallel.c
@@ -56,11 +56,12 @@ void task_argument(void *node)
 
 	pthread_mutex_lock(&nrNodes_mutex);
 	nrNodes--;
-	pthread_mutex_unlock(&nrNodes_mutex);
-
-	if (nrNodes <= 1) {
+	
+	if (nrNodes <= 0) {
 		tp->noTaskLeft = 1;
 	}
+
+	pthread_mutex_unlock(&nrNodes_mutex);
 }
 
 static void process_node(unsigned int idx)
